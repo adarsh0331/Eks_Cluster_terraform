fe76e99994e7a45c512613e34f2c2b29d3b6222c
Barkunta Adarsh <129056162+adarsh0331@users.noreply.github.com>
2025-03-28 13:37:12 +0530

    Update main.tf

--- eks-install/main.tf
+++ eks-install/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "cluster1-terraform-eks-state-s3-bucket"
+    bucket         = "cluster1-terraform-eks-state-s3-bucket1"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-eks-state-locks"
