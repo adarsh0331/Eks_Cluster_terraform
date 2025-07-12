b0a29ec6a39b234fa51b388ee220144682607dfd
Barkunta Adarsh <129056162+adarsh0331@users.noreply.github.com>
2025-07-12 15:14:38 +0530

    Update main.tf

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "cluster1-terraform-eks-state-s3-bucket1"
+  bucket = "cluster1-terraform-eks-state-s3-bucket2"
 
   lifecycle {
     prevent_destroy = false
